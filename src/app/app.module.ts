962198d7e236d871ba0d2f6bab907991c9f05660
Marco <marcosuarezpetrillo@hotmail.com>
2018-10-12 19:23:41 -0400

    modules creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Route } from '@angular/router';
+
+import { AlertModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { DisplayComponent } from './display/display.component';
+import { ProfileComponent } from './profile/profile.component';
+
+const routes: Route[] = [
+  {
+    path: '',
+    component: DisplayComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    DisplayComponent,
+    ProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AlertModule.forRoot(),
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
